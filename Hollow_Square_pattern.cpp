bf282128e1cd4c50105e06882ccf0a1b950d97a1
Sky <122771939+Sky1337x@users.noreply.github.com>
2023-08-30 00:30:28 +0530

    added Hollow Square Pattern code

---
+++ Hollow_Square_pattern.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int side;
+    cout<<"enter size of side: ";
+    cin>>side;
+
+    for(int i=0;i<side;i++){
+        for(int j=0;j<side;j++){
+            if(i==0||i==side-1){
+                cout<<"* ";   
+            }
+            else{
+                if(j==0||j==side-1){
+                    cout<<"* ";
+                }
+                else{
+                    cout<<"  ";
+                }
+                
+            }
+        }
+        cout<<endl;
+    }
+    
+ 
+}
